60deb2efe7b0ac31a3058df26f06df7a3bd440f3
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-01 19:30:21 +0500

    Room

--- CodeAlpha_Hotel Reservation System/src/HotelReservationSystem.java
+++ CodeAlpha_Hotel Reservation System/src/HotelReservationSystem.java
@@ -7,3 +7,10 @@ class Room {
     double price;
     boolean isBooked;
 
+    public Room(int roomNumber, String category, double price) {
+        this.roomNumber = roomNumber;
+        this.category = category;
+        this.price = price;
+        this.isBooked = false; 
+    }
+}
