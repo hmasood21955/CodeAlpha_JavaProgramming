4f11664848412592bb5a2dcf114cdfde9130e653
Muhammad Masood Hussain <144346171+hmasood21955@users.noreply.github.com>
2025-03-02 14:12:50 +0500

    Reservation

--- CodeAlpha_Hotel Reservation System/src/HotelReservationSystem.java
+++ CodeAlpha_Hotel Reservation System/src/HotelReservationSystem.java
@@ -14,3 +14,19 @@ class Room {
         this.isBooked = false; 
     }
 }
+
+// Reservation class to manage bookings
+class Reservation {
+    String guestName;
+    Room room;
+    int nights;
+    double totalCost;
+
+    public Reservation(String guestName, Room room, int nights) {
+        this.guestName = guestName;
+        this.room = room;
+        this.nights = nights;
+        this.totalCost = room.price * nights;
+        room.isBooked = true; // Mark the room as booked
+    }
+}
